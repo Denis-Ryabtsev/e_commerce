ffcaac32ce185ba0d09b1bd98ea9dd31b8743bcf
lazarus <ryabtsev.denis05@gmail.com>
2024-10-30 17:13:08 +0700

    Added user routers (login, logout, registr)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
 from fastapi import FastAPI
 
+from auth.base_config import fastapi_users, auth_backend
+from auth.schemas import UserCreate, UserRead
 
-app = FastAPI()
+
+app = FastAPI(
+    title="E-commerce prog"
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
