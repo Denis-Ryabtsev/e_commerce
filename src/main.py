f54c8032f0476510a0a131016b082fe1a7df567a
lazarus <ryabtsev.denis05@gmail.com>
2024-11-01 14:35:18 +0700

    Code review and change input fields in login proccess

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 
 from auth.base_config import fastapi_users, auth_backend
-from auth.schemas import UserCreate, UserRead
 from auth.router import router_reg
-from database import get_async_session
 
 
 app = FastAPI(
     title="E-commerce prog"
 )
 
+# authentication router
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     #prefix="/auth",
     tags=["Authentification"],
 )
 
-
+# Registration router
 app.include_router(router_reg)
