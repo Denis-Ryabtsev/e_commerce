72ff067707ec3bb42c98b8452f3b9e0bb2b2e306
lazarus <ryabtsev.denis05@gmail.com>
2024-11-01 14:29:04 +0700

    Code review

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -17,15 +17,15 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=setting.SECRET_AUTH, 
         lifetime_seconds=3600
-        )
+    )
 
 auth_backend = AuthenticationBackend(
-    name="jwt",
-    transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
-)
+        name="jwt",
+        transport=cookie_transport,
+        get_strategy=get_jwt_strategy,
+    )
 
 fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
+        get_user_manager,
+        [auth_backend],
+    )
