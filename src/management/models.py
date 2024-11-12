e92e10c65b73867808abc07bd4757b9b3a4bc52f
lazarus <ryabtsev.denis05@gmail.com>
2024-11-12 09:31:48 +0700

    added models for new tables

---
+++ src/management/models.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+
+from database import Base
+
+
+class Good(Base):
+    __tablename__ = 'good'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True
+    )
+    product_name: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    seller_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id', ondelete='CASCADE')
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey('category.id')
+    )
+    unit_price: Mapped[float] = mapped_column(
+        nullable=False
+    )
+
+    users = relationship(
+        'User', 
+        back_populates='goods'
+    )
+    categories = relationship(
+        'Category', 
+        back_populates='goods'
+    )
+    od = relationship(
+        'OrderDetail',
+        back_populates='goods'
+    )
+
+class Order(Base):
+    __tablename__ = 'order'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True
+    )
+    seller_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id")
+    )
+    customer_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id")
+    )
+    date_order: Mapped[datetime]
+    ship_country: Mapped[str] = mapped_column(
+        nullable=False
+    )
+
+    seller = relationship(
+        'User', 
+        foreign_keys=[seller_id], 
+        back_populates='ord_sel'
+    )
+    customer = relationship(
+        'User', 
+        foreign_keys=[customer_id], 
+        back_populates='ord_cus'
+    )
+
+    od = relationship(
+        'OrderDetail',
+        back_populates='orders'
+    )
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True
+    )
+    category_name: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    description: Mapped[str]
+
+    goods = relationship(
+        'Good', 
+        back_populates='categories'
+    )
+
+
+class OrderDetail(Base):
+    __tablename__ = 'order_detail'
+
+    order_id: Mapped[int] = mapped_column(
+        ForeignKey('order.id'), primary_key=True
+    )
+    good_id: Mapped[int] = mapped_column(
+        ForeignKey('good.id'), primary_key=True
+    )
+    quantity: Mapped[int] = mapped_column(
+        nullable=False
+    )
+    price: Mapped[float] = mapped_column(
+        nullable=False
+    )
+
+    orders = relationship(
+        'Order',
+        back_populates='od'
+    )
+    goods = relationship(
+        'Good',
+        back_populates='od'
+    )
