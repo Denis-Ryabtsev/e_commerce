8a2f010ada4b4ea6fe7a3f410cf1481b510a519e
lazarus <ryabtsev.denis05@gmail.com>
2024-12-02 10:47:06 +0700

    added fixture for creating test categories and users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 import asyncio
 import pytest
 
-from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from fastapi_users.jwt import generate_jwt
 from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users.password import PasswordHelper
 
-from database import get_async_session, a_engine, Base
+from database import get_async_session, Base
 from config import setting
 from auth.models import User
+from management.models import Category
 from auth.schemas import UserCreate
 from auth.manager import UserManager
 from main import app
@@ -16,30 +17,28 @@ from main import app
 
 engine_test = create_async_engine(
     url=setting.DB_URL,
-    pool_size=5,
+    pool_size=10,
     max_overflow=10, 
     echo=False
 )
 
 engine_db = create_async_engine(
     url=setting.DB_URL,
-    pool_size=5,
+    pool_size=10,
     max_overflow=10,  
     echo=False
 )
 
 AsyncSessionTest = async_sessionmaker(engine_test)
-# user_session = async_sessionmaker(engine_test)
+user_session = async_sessionmaker(engine_test)
 
+password = PasswordHelper()
 
 @pytest.fixture(scope="session", autouse=True)
 async def setup_db():
-
     async with engine_db.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
     yield 
-
     async with engine_db.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
@@ -50,6 +49,10 @@ def event_loop():
     yield loop
     loop.close()
 
+async def session_user():
+    async with user_session() as session:
+        yield session
+
 @pytest.fixture(scope="function", autouse=True)
 def override_get_async_session():
     async def _get_test_session():
@@ -59,37 +62,47 @@ def override_get_async_session():
     app.dependency_overrides[get_async_session] = _get_test_session
     return _get_test_session
 
-#   Создание нужно будет сюда перенести, чтобы не создавать в каждом тестовом файле пользователей (тесты товаров будут в другом файле, а создание юзеров происходит только в test_auth)
-
 @pytest.fixture
 async def user_manager(override_get_async_session):
-    """Создает экземпляр UserManager для использования в тестах."""
-    # Получаем сессию из override_get_async_session
     async for session in override_get_async_session():
-        user_db = SQLAlchemyUserDatabase(session, User)  # Создаём SQLAlchemyUserDatabase
+        user_db = SQLAlchemyUserDatabase(session, User) 
         return UserManager(user_db=user_db)
 
-from fastapi_users.password import PasswordHelper
-password = PasswordHelper()
-
 @pytest.fixture()
-async def test_user(override_get_async_session):
-    hashed = password.hash("Bb3##")
-    async for session in override_get_async_session():
+async def test_user():
+    async for session in session_user():
         user = [
             User(
-                username='testver',
-                email="ver@gmail.com",
-                hashed_password=hashed,
-                role='seller',
+                username='custver',
+                email="custver@gmail.com",
+                hashed_password=password.hash("Bb3##"),
+                role='customer',
+                is_active=True,
+                is_superuser=False,
+                is_verified=True
+            ),
+            User(
+                username='custnot',
+                email="custnot@gmail.com",
+                hashed_password=password.hash("Bb1!!"),
+                role='customer',
                 is_active=True,
                 is_superuser=False,
                 is_verified=False
             ),
             User(
-                username='testver2',
-                email="ver2@gmail.com",
-                hashed_password=hashed,
+                username='sellver',
+                email="sellver@gmail.com",
+                hashed_password=password.hash("Bb2@@"),
+                role='seller',
+                is_active=True,
+                is_superuser=False,
+                is_verified=True
+            ),
+            User(
+                username='sellnot',
+                email="sellnot@gmail.com",
+                hashed_password=password.hash("Bb4$$"),
                 role='seller',
                 is_active=True,
                 is_superuser=False,
@@ -97,10 +110,24 @@ async def test_user(override_get_async_session):
             )
         ]
 
-
+        categories = [
+            Category(
+                category_name="beverages",
+                description="Coffee, teas, beers"
+            ),
+            Category(
+                category_name="confections",
+                description="Desserts, candies, and sweet breads"
+            ),
+            Category(
+                category_name="cars",
+                description="Japan and American cars"
+            ),
+            Category(
+                category_name="games",
+                description="Computer games"
+            )
+        ]
         
-        session.add_all(user)
+        session.add_all(user + categories)
         await session.commit()
-
-
-    
